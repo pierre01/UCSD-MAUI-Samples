e66d75faf18cacdebc0b72561b14260426a52c6e
Pierre <pierre@delange.design>
2022-05-22 22:58:34 -0700

    Update to controls

--- MauiControls/Views/StepAndProgress.xaml.cs
+++ MauiControls/Views/StepAndProgress.xaml.cs
@@ -9,7 +9,9 @@ public partial class StepAndProgress : ContentPage
 
     private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
     {
-        SliderLabel.Text = e.NewValue.ToString();
+        SliderLabel.Text = e.NewValue.ToString("F2");
+        ProgressBarExample.Progress = (e.NewValue -5)/10;
+        ProgressBarLabel.Text = $"{ProgressBarExample.Progress * 100:F2}%";
     }
 
     private void Stepper_ValueChanged(object sender, ValueChangedEventArgs e)
