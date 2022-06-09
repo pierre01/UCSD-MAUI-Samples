310da5d62bd88d63878c9184f2925ea2416177d9
Pierre <pierre@delange.design>
2022-06-08 22:31:53 -0700

    DataBinding Samples

--- MauiDataBinding/Views/ObjectBindingPage.xaml.cs
+++ MauiDataBinding/Views/ObjectBindingPage.xaml.cs
@@ -8,22 +8,3 @@ public partial class ObjectBindingPage : ContentPage
         BindingContext = new Person(DateTime.Parse("03/14/1879"), "Albert", "Einstein", "Male");
 	}
 }
-
-public class Person
-{
-    public Person(DateTime dateOfBirth, string firstName, string lastName, string gender)
-    {
-        DateOfBirth = dateOfBirth;
-        Age = (DateTime.Today - dateOfBirth).Days/365;
-        FirstName = firstName;
-        LastName = lastName;
-        Gender = gender;
-    }
-    
-    public DateTime DateOfBirth { get; set; }
-    public int Age { get; set; }
-
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Gender { get; set; } = string.Empty;
-}
---
+++ MauiDataBinding/Views/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace MauiDataBinding.Views;
+
+public class Person
+{
+    public Person(DateTime dateOfBirth, string firstName, string lastName, string gender)
+    {
+        DateOfBirth = dateOfBirth;
+        Age = (DateTime.Today - dateOfBirth).Days/365;
+        FirstName = firstName;
+        LastName = lastName;
+        Gender = gender;
+    }
+    
+    public DateTime DateOfBirth { get; set; }
+    public int Age { get; set; }
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Gender { get; set; } = string.Empty;
+}
