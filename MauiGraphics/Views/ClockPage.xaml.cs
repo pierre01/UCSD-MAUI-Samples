2833efdc0bf8712157e7e235b8ee063efd67bb52
Pierre <pierre@delange.design>
2022-07-12 22:27:04 -0700

    More Graphics Samples plus a live clock from MS sample

---
+++ MauiGraphics/Views/ClockPage.xaml.cs
@@ -0,0 +1,37 @@
+namespace MauiGraphics.Views;
+
+public partial class ClockPage : ContentPage
+{
+    PeriodicTimer _clock;
+
+    public ClockPage()
+	{
+		InitializeComponent();
+	}
+
+    protected override void OnDisappearing()
+    {
+        _clock.Dispose();
+        _clock = null;        
+        
+        base.OnDisappearing();
+
+    }
+
+    protected override async void OnAppearing()
+    {
+        if (_clock == null)
+        {
+            _clock = new PeriodicTimer(TimeSpan.FromSeconds(1));
+        }        
+        
+        base.OnAppearing();
+
+        while (await _clock.WaitForNextTickAsync())
+        {
+            myGraphicsView.Invalidate();
+        }
+
+    }
+
+}
--- MauiGraphics/Views/DrawableArea.cs
+++ MauiGraphics/Views/DrawableArea.cs
@@ -10,28 +10,36 @@ public class DrawableArea : IDrawable
 {
     public void Draw(ICanvas canvas, RectF dirtyRect)
     {
+        var d = DeviceDisplay.Current.MainDisplayInfo.Density;
+        var w = DeviceDisplay.Current.MainDisplayInfo.Width;
+        var h = DeviceDisplay.Current.MainDisplayInfo.Height;
         Random rnd = new Random();
         // Drawing goes here
-        canvas.StrokeSize = 8;        
-        canvas.StrokeLineCap = LineCap.Round;        
+        canvas.StrokeSize = 16;        
+        canvas.StrokeLineCap = LineCap.Round;
+        canvas.StrokeColor = Colors.Yellow;
+        canvas.DrawRectangle(0, 0, 400, 400);
         
+        //canvas.Scale(canvas.DisplayScale, canvas.DisplayScale);
+
         // Backgtound tracks
-        canvas.StrokeColor = Colors.LightGray;
-        canvas.DrawArc(10, 10, 100, 100, 90, 95, true, false);
-        canvas.DrawArc(20, 20, 80, 80, 90,   95, true, false);
-        canvas.DrawArc(30, 30, 60, 60, 90,   95, true, false);        
+        canvas.StrokeColor = Color.FromArgb("#b4cccccc");
+        canvas.DrawArc(100, 100, 200, 200, 90, 95, true, false);
+        canvas.DrawArc(120, 120, 160, 160, 90,   95, true, false);
+        canvas.DrawArc(140, 140, 120, 120, 90,   95, true, false);        
         
         // Track 1
         canvas.StrokeColor = Colors.Green;
-        canvas.DrawArc(10, 10, 100, 100, 90, rnd.Next(90,360),true, false);
+        canvas.DrawArc(100, 100, 200, 200, 90, rnd.Next(90,360),true, false);
         // Track 2
         canvas.StrokeColor = Colors.Orange;                 
-        canvas.DrawArc(20, 20, 80, 80, 90, rnd.Next(90, 360), true, false);
+        canvas.DrawArc(120, 120, 160, 160, 90, rnd.Next(90, 360), true, false);
         // Track 3
         canvas.StrokeColor = Colors.Red;                  
-        canvas.DrawArc(30, 30, 60, 60, 90, rnd.Next(90, 360), true, false);
+        canvas.DrawArc(140, 140, 120, 120, 90, rnd.Next(90, 360), true, false);
         
-        
+         
+       
 
 
     }
