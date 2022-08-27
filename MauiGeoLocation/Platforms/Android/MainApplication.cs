9b14310ec5639e02b086b747696138250024ca57
Pierre <pierre@delange.design>
2022-08-27 03:26:57 -0700

    Added camera permission

--- MauiGeoLocation/Platforms/Android/MainApplication.cs
+++ MauiGeoLocation/Platforms/Android/MainApplication.cs
@@ -3,6 +3,7 @@ using Android.Runtime;
 
 [assembly: UsesPermission(Android.Manifest.Permission.AccessCoarseLocation)]
 [assembly: UsesPermission(Android.Manifest.Permission.AccessFineLocation)]
+[assembly: UsesPermission(Android.Manifest.Permission.Camera)]
 [assembly: UsesFeature("android.hardware.location", Required = false)]
 [assembly: UsesFeature("android.hardware.location.gps", Required = false)]
 [assembly: UsesFeature("android.hardware.location.network", Required = false)]
