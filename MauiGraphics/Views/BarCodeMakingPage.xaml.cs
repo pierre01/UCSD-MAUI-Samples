cedf556a19b0ea8298eb42203905555d7606c5fd
pierre01 <pierre@delange.design>
2022-11-28 15:15:02 -0800

    Encoded VCard and moved to .net7

--- MauiGraphics/Views/BarCodeMakingPage.xaml.cs
+++ MauiGraphics/Views/BarCodeMakingPage.xaml.cs
@@ -4,8 +4,15 @@ public partial class BarCodeMakingPage : ContentPage
 {
 	public BarCodeMakingPage()
 	{
-		InitializeComponent();
-	}
+		InitializeComponent(); 
+        var vcard=@"BEGIN:VCARD
+N:Huguet;Pierre;;;M.Sc.
+BDAY:--0531
+EMAIL;TYPE=work:phuguet@ucsd.edu
+URL:https://delange.design
+END:VCARD";
+        BarcodeDisplayControl.Value = vcard;
+    }
 
 	private void QRCodeChanged(object sender, TextChangedEventArgs e)
 	{
