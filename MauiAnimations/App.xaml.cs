0e913a8222fbcb00b79536d2e74df15a2f47f6e5
pierre01 <pierre@delange.design>
2022-12-28 01:14:11 -0800

    Created a set for basic animations

--- MauiAnimations/App.xaml.cs
+++ MauiAnimations/App.xaml.cs
@@ -8,5 +8,19 @@
 
             MainPage = new AppShell();
         }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            const int newWidth = 800;
+            const int newHeight = 700;
+            window.X = 500;
+            window.Y = 200;
+            window.Width = newWidth;
+            window.Height = newHeight;
+
+            return window;
+        }
     }
 }
--- MauiAnimations/Views/BasicAnimationsView.xaml.cs
+++ MauiAnimations/Views/BasicAnimationsView.xaml.cs
@@ -1,9 +1,39 @@
+using Microsoft.Maui.Controls.Shapes;
+
 namespace MauiAnimations.Views;
 
-public partial class BasicAnimations : ContentPage
+public partial class BasicAnimationsView : ContentPage
 {
-	public BasicAnimations()
+	public BasicAnimationsView()
 	{
 		InitializeComponent();
+        PowLabel.Opacity=0;    
 	}
+
+    private int _shotTriggered = 0;
+    private async void OnShoot(object sender, EventArgs e)
+    {
+        PowLabel.Opacity=1;
+        if (_shotTriggered >= 8)
+        {
+            PowLabel.Text = "Click!";
+        }
+        else
+        {
+            var cartridge = BarrelGrid.Children[_shotTriggered] as VisualElement;
+            cartridge.FadeTo(.5, 400, Easing.CubicIn);
+
+        }
+        await Task.WhenAll
+        (
+             
+            //TextToSpeech.Default.SpeakAsync(timeLeft),
+            PowLabel.FadeTo(0, 400, Easing.CubicIn),
+            PowLabel.ScaleTo(5, 400, Easing.CubicOut),
+
+            BarrelGrid.RelRotateTo(45,600)
+        );
+        PowLabel.ScaleTo(1, 40);
+        _shotTriggered++;
+    }
 }
