88eccc41e7eb546206cac669184b63020f7a2b0f
pierre01 <pierre@delange.design>
2023-01-08 23:10:59 -0800

    Added reload button for animation sample

--- MauiAnimations/Views/BasicAnimationsView.xaml.cs
+++ MauiAnimations/Views/BasicAnimationsView.xaml.cs
@@ -8,14 +8,17 @@ public partial class BasicAnimationsView : ContentPage
 	{
 		InitializeComponent();
         PowLabel.Opacity=0;    
+        ReloadButton.TranslateTo(300,0,100,Easing.CubicOut);
 	}
 
     private int _shotTriggered = 0;
     private async void OnShoot(object sender, EventArgs e)
     {
+        ShootButton.IsEnabled = false;
         PowLabel.Opacity=1;
         if (_shotTriggered >= 8)
         {
+            // We emptied the barrel 
             PowLabel.Text = "Click!";
         }
         else
@@ -33,7 +36,36 @@ public partial class BasicAnimationsView : ContentPage
 
             BarrelGrid.RelRotateTo(45,600)
         );
-        PowLabel.ScaleTo(1, 40);
         _shotTriggered++;
+        if (_shotTriggered == 8)
+        {
+            // Animate Reload button
+            ReloadButton.TranslateTo(0,0,500,Easing.CubicIn);
+
+            ReloadButton.IsEnabled = true;
+        }
+        ShootButton.IsEnabled = true;
+        PowLabel.ScaleTo(1, 40);
+    }
+
+    /// <summary>
+    /// Reload button has been clicked...
+    /// Change the cartridges back to new
+    /// </summary>
+    private void OnReload(object sender, EventArgs e)
+    {
+        // reload then animate away
+        ReloadButton.IsEnabled = false;
+        ShootButton.IsEnabled = false;
+        for (int i = 0; i <= 8; i++)
+        {
+            var cartridge = BarrelGrid.Children[i] as VisualElement;
+            // Reactivate cartridges
+            cartridge.FadeTo(1, 100, Easing.CubicIn);
+        }
+        ReloadButton.TranslateTo(300,0,800,Easing.CubicOut);
+        PowLabel.Text = "Bang!";
+        _shotTriggered = 0;
+        ShootButton.IsEnabled = true;
     }
 }
