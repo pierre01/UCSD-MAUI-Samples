495273a9893a66309b85cf79a214e14a70c91bdf
pierre01 <pierre@delange.design>
2023-01-08 23:13:22 -0800

    Windows page layout

--- MauiLayouts/App.xaml.cs
+++ MauiLayouts/App.xaml.cs
@@ -8,5 +8,19 @@
 
             MainPage = new AppShell();
         }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            const int newWidth = 600;
+            const int newHeight = 1000;
+
+            window.X = 500;
+            window.Y = 500;
+            window.Width = newWidth;
+            window.Height = newHeight;
+            window.MinimumHeight = newHeight;
+            return window;
+        }
     }
 }
