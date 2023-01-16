14aa7df1d7476afb2604cf59a59767ee1597ec46
pierre01 <pierre@delange.design>
2023-01-15 20:25:45 -0800

    Added toast and navigation

--- MauiNavigation/App.xaml.cs
+++ MauiNavigation/App.xaml.cs
@@ -20,6 +20,38 @@ public partial class App : Application
         window.Width = newWidth;
         window.Height = newHeight;
 
+        window.Created += Window_Created;
+        window.Activated += Window_Activated;
+        window.Deactivated += Window_Deactivated;
+        window.Stopped += Window_Stopped;
+        window.Resumed += Window_Resumed;
+        window.Destroying += Window_Destroying;
+
+
         return window;
+    }
+
+    private void Window_Destroying(object sender, EventArgs e)
+    {
+    }
+
+    private void Window_Resumed(object sender, EventArgs e)
+    {
+    }
+
+    private void Window_Stopped(object sender, EventArgs e)
+    {
+    }
+
+    private void Window_Deactivated(object sender, EventArgs e)
+    {
+    }
+
+    private void Window_Activated(object sender, EventArgs e)
+    {
+    }
+
+    private void Window_Created(object sender, EventArgs e)
+    {
     }
 }
--- MauiNavigation/MauiProgram.cs
+++ MauiNavigation/MauiProgram.cs
@@ -9,14 +9,13 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
 
-        // Initialise the toolkit
-        builder.UseMauiApp<App>().UseMauiCommunityToolkit();
 
         return builder.Build();
 	}
