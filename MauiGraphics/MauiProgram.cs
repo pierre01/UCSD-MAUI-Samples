ed5b349d482689ba364fcc011f89db46ae634f38
pierre01 <pierre@delange.design>
2023-01-15 22:09:29 -0800

    Update dependencies

--- MauiGraphics/MauiProgram.cs
+++ MauiGraphics/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using SkiaSharp.Views.Maui.Controls.Hosting;
+﻿using CommunityToolkit.Maui;
+using SkiaSharp.Views.Maui.Controls.Hosting;
 using ZXing.Net.Maui.Controls;
 
 namespace MauiGraphics;
@@ -8,20 +9,22 @@ public static class MauiProgram
 	public static MauiApp CreateMauiApp()
 	{
 		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.UseBarcodeReader()
+        builder
+            .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
+            .UseBarcodeReader()
             .UseSkiaSharp()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			})
-		#region fix
-			.ConfigureMauiHandlers(h =>
-			{
-				//h.AddHandler(typeof(ZXing.Net.MAUI.Controls.Camerabase))
-			});
+
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            })
+        #region fix
+            .ConfigureMauiHandlers(h =>
+            {
+                //h.AddHandler(typeof(ZXing.Net.MAUI.Controls.Camerabase))
+            });
 		#endregion
 
 		return builder.Build();
