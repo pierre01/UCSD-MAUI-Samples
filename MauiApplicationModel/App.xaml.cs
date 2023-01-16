05947809b9a5d5c4918e567049d2ab6dae29eec6
pierre01 <pierre@delange.design>
2023-01-15 20:24:14 -0800

    Added events placeholders

--- MauiApplicationModel/App.xaml.cs
+++ MauiApplicationModel/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiApplicationModel;
+﻿using PdfKit;
+
+namespace MauiApplicationModel;
 
 public partial class App : Application
 {
@@ -8,6 +10,23 @@ public partial class App : Application
 
 		MainPage = new AppShell();
 	}
+
+
+    protected override void OnStart()
+    {
+        base.OnStart();
+    }
+
+    protected override void OnSleep()
+    {
+        base.OnSleep();
+    }
+
+    protected override void OnResume()
+    {
+        base.OnResume();
+    }
+
 
     protected override Window CreateWindow(IActivationState activationState)
     {
@@ -20,6 +39,38 @@ public partial class App : Application
         window.Width = newWidth;
         window.Height = newHeight;
 
+        window.Created += Window_Created;
+        window.Activated += Window_Activated;
+        window.Deactivated += Window_Deactivated;
+        window.Stopped += Window_Stopped;
+        window.Resumed += Window_Resumed;
+        window.Destroying += Window_Destroying;
+
+
         return window;
+    }
+
+    private void Window_Destroying(object sender, EventArgs e)
+    {
+    }
+
+    private void Window_Resumed(object sender, EventArgs e)
+    {
+    }
+
+    private void Window_Stopped(object sender, EventArgs e)
+    {
+    }
+
+    private void Window_Deactivated(object sender, EventArgs e)
+    {
+    }
+
+    private void Window_Activated(object sender, EventArgs e)
+    {
+    }
+
+    private void Window_Created(object sender, EventArgs e)
+    {
     }
 }
