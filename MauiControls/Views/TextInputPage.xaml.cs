0af4b4b024d27c74a1449139897bee3393a725e3
Pierre Huguet <pierre@delange.design>
2024-04-10 20:44:28 -0700

    Better examples for the Text Inputs

--- MauiControls/Views/TextInputPage.xaml.cs
+++ MauiControls/Views/TextInputPage.xaml.cs
@@ -2,15 +2,25 @@ namespace MauiControls.Views;
 
 public partial class TextInputPage : ContentPage
 {
-	public TextInputPage()
-	{
-		InitializeComponent();
-	}
+    public TextInputPage()
+    {
+        InitializeComponent();
+    }
 
-    private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+    //https://learn.microsoft.com/en-us/dotnet/maui/user-interface/controls/entry?view=net-maui-8.0
+
+    private void Name_TextChanged(object sender, TextChangedEventArgs e)
     {
         var v1 = e.NewTextValue;
         var v2 = e.OldTextValue;
+        NameLabel.Text = v1;
+    }
+
+    private void Age_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        var v1 = e.NewTextValue;
+        var v2 = e.OldTextValue;
+        AgeLabel.Text = v1;
     }
 
     private void Editor_TextChanged(object sender, TextChangedEventArgs e)
