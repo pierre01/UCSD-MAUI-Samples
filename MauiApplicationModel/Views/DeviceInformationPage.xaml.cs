bcc403ed97a1d4557591952f2d2019efda056232
Pierre Huguet <pierre@delange.design>
2024-05-10 17:10:25 -0700

    Added Screen Info

--- MauiApplicationModel/Views/DeviceInformationPage.xaml.cs
+++ MauiApplicationModel/Views/DeviceInformationPage.xaml.cs
@@ -1,17 +1,18 @@
 namespace MauiApplicationModel.Views;
 
+
 public partial class DeviceInformationPage : ContentPage
 {
-	public DeviceInformationPage()
-	{
-		InitializeComponent();
+    public DeviceInformationPage()
+    {
+        InitializeComponent();
         ReadDeviceInfo();
-	}
+    }
 
     private void ReadDeviceInfo()
     {
         System.Text.StringBuilder sb = new System.Text.StringBuilder();
-        
+
         sb.AppendLine($"Name:    {AppInfo.Current.Name}");
         sb.AppendLine($"Package: {AppInfo.Current.PackageName}");
         sb.AppendLine($"Version: {AppInfo.Current.VersionString}");
@@ -23,9 +24,15 @@ public partial class DeviceInformationPage : ContentPage
         sb.AppendLine($"Manufacturer: {DeviceInfo.Current.Manufacturer}");
         sb.AppendLine($"Name:         {DeviceInfo.Name}");
         sb.AppendLine($"OS Version:   {DeviceInfo.VersionString}");
-        sb.AppendLine($"Refresh Rate: {DeviceInfo.Current}");
         sb.AppendLine($"Idiom:        {DeviceInfo.Current.Idiom}");
         sb.AppendLine($"Platform:     {DeviceInfo.Current.Platform}");
+        sb.AppendLine();
+        sb.AppendLine($"Pixel width: {DeviceDisplay.Current.MainDisplayInfo.Width} / Pixel Height: {DeviceDisplay.Current.MainDisplayInfo.Height}");
+        sb.AppendLine($"Density: {DeviceDisplay.Current.MainDisplayInfo.Density}");
+        sb.AppendLine($"Orientation: {DeviceDisplay.Current.MainDisplayInfo.Orientation}");
+        sb.AppendLine($"Rotation: {DeviceDisplay.Current.MainDisplayInfo.Rotation}");
+        sb.AppendLine($"Refresh Rate: {DeviceDisplay.Current.MainDisplayInfo.RefreshRate}");
+
 
         bool isVirtual = DeviceInfo.Current.DeviceType switch
         {
